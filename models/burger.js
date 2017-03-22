c01ba04201eb3f79b8b58b3aea2167ab09fd0bf6
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-03-22 17:01:24 -0400

    updating files

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,32 @@
 var orm = require("../config/orm.js");
 
-var router = express.Router();
+var burger = {
 
+	all: function(cb){
+		orm.all("burgers", function(res){
+			cb(res);
+		});
+	},
+
+	add: function(value, cb){
+		orm.add("burgers", "burger_name", value, function(res){
+			cb(res);
+		});
+	},
+
+	update: function(columnValue, conditionalValue, cb){
+		orm.update("burgers", "devoured", columnValue, "id", conditionalValue, function(res){
+			cb(res);
+		});
+	},
+
+	delete: function (conditionalValue, cb){
+		orm.delete("burgers", "id", conditionalValue, function(res){
+			cb(res);
+		});
+	},
+
+};
 
 
 module.exports = burger;
