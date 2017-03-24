ec92d5fadb5dc6ee4e4e68146ffeb1c82190c306
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-03-24 13:51:20 -0400

    commiting changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -24,4 +24,4 @@ var burgersController = require("./controllers/burgersController.js");
 
 app.use("/", burgersController);
 
-app.listen(port);
+app.listen(PORT);
