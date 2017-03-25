e124d1eb9746687c755cc9aa4b59c0b3d5cc91d9
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-03-25 10:48:47 -0400

    editting connection file

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var app = require('../server');
 
-var connection = mysql.createConnection({
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
+console.log('--------------the environment we are using----------------');
+console.log(app.settings.env);
+console.log('--------------the environment we are using----------------');
 
+if (app.settings.env == 'development'){
+  var connection = mysql.createConnection({
+    port: 8889,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "groupon_db"
+  });
+}else {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
